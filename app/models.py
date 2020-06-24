ad21bc990a34ad4a2695109875e0b01ed692879f
Вера <integragirl@mail.ru>
2020-06-24 15:50:15 +0500

    deploy

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.translation import gettext as _
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField
+    status = models.CharField(max_length=10, choices=[('D', 'draft'), ('P', 'published')])
+    content = models.TextField()
+    updated = models.DateTimeField(default=timezone.now)
+    publication_date = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL,
+                                verbose_name=_("Категория"),
+                                related_name="post_category",
+                                default=None,
+                                blank=True,
+                                null=True,
+                                )
+    author = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField
+
+    def __str__(self):
+        return self.name
